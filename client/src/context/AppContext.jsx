7eb2797d6d48c6778d6a34801bfb64e2b2f33996
priyansh-2002 <priyansh.hss02@gmail.com>
2026-01-01 14:09:56 +0530

    context and authentication

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,168 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { dummyProducts } from "../assets/assets.js";
+import toast from "react-hot-toast";
+import axios from "../api/axios";
+import api from "../api/axios";
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
+export const AppContext = createContext(null);
+
+export const AppContextProvider = ({ children }) => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+  const [isSeller, setIsSeller] = useState(false);
+  const [showUserLogin, setShowUserLogin] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [cartItems, setCartItems] = useState({});
+  const [searchQuery, setSearchQuery] = useState("");
+
+  // check seller status
+  const fetchSeller = async () => {
+    try {
+      const { data } = await api.get("/api/seller/is-auth");
+      if (data.success) {
+        setIsSeller(true);
+      } else {
+        setIsSeller(false);
+      }
+    } catch (error) {
+      setIsSeller(false);
+    }
+  };
+
+  // fetch user auth status ,user Data and cart items
+  const fetchUser = async () => {
+    try {
+      const { data } = await api.get("/api/user/is-auth");
+      if (data.success) {
+        setUser(data.user);
+        setCartItems(data.user.cart);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      // toast.error(error.message);
+      setUser(null);
+    }
+  };
+
+  // fetch products
+  const fetchProducts = async () => {
+    try {
+      const { data } = await api.get("/api/product");
+      if (data.success) {
+        setProducts(data.products);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  // add product to cart
+  const addToCart = (itemId) => {
+    let cartData = structuredClone(cartItems || {}); // safeguard for undefined
+
+    if (cartData[itemId]) {
+      cartData[itemId] += 1;
+    } else {
+      cartData[itemId] = 1;
+    }
+
+    setCartItems(cartData);
+    toast.success("Added to cart");
+  };
+
+  // update cart item quantity
+  const updateCartItem = (itemId, quantity) => {
+    let cartData = structuredClone(cartItems);
+    cartData[itemId] = quantity;
+    setCartItems(cartData);
+    toast.success(`cart updated`);
+  };
+
+  // total cart items
+  const cartCount = () => {
+    let totalCount = 0;
+    for (const item in cartItems) {
+      totalCount += cartItems[item];
+    }
+    return totalCount;
+  };
+
+  // total cart amount
+  const totalCartAmount = () => {
+    let totalAmount = 0;
+    for (const items in cartItems) {
+      let itemInfo = products.find((product) => product._id === items);
+      if (cartItems[items] > 0) {
+        totalAmount += cartItems[items] * itemInfo.offerPrice;
+      }
+    }
+    return Math.floor(totalAmount * 100) / 100;
+  };
+
+  // remove product from cart
+  const removeFromCart = (itemId) => {
+    let cartData = structuredClone(cartItems);
+    if (cartData[itemId]) {
+      cartData[itemId] -= 1;
+      if (cartData[itemId] === 0) {
+        delete cartData[itemId];
+      }
+      toast.success(`remove from cart`);
+      setCartItems(cartData);
+    }
+  };
+
+  useEffect(() => {
+    fetchSeller();
+    fetchProducts();
+    fetchUser();
+  }, []);
+
+  // update database cart items
+  useEffect(() => {
+  if (!user) return;
+
+  const updateCart = async () => {
+    try {
+      const { data } = await api.post("/api/cart/update", { cartItems });
+      if (!data.success) toast.error(data.message);
+    } catch (err) {
+      toast.error(err.response?.data?.message || "Cart update failed");
+    }
+  };
+
+  updateCart();
+  }, [cartItems, user]);
+
+  const value = {
+    navigate,
+    user,
+    setUser,
+    isSeller,
+    setIsSeller,
+    showUserLogin,
+    setShowUserLogin,
+    products,
+    cartItems,
+    addToCart,
+    updateCartItem,
+    removeFromCart,
+    searchQuery,
+    setSearchQuery,
+    cartCount,
+    totalCartAmount,
+    // axios,
+    fetchProducts,
+    setCartItems,
+  };
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
+
+export const useAppContext = () => {
+  return useContext(AppContext);
+};
